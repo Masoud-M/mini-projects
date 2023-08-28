ee658526db9d5060d49256385dec69e51a00ab43
Masoud-M <masoudmoghaddam999@gmail.com>
2023-08-28 13:45:27 +0330

    refactored the code to use useReducer hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import JumpOnFocus from "./components/JumpOnFocus";
 import CurrencyConverter from "./components/currency-converter/CurrencyConverter";
+import ReactQuiz from "./components/react-quiz/ReactQuiz";
 import SplitTheBill from "./components/splitthebill/SplitTheBill";
 import TipCalculator from "./components/tip-calculator/TipCalculator";
-
+import Geolocation from "./components/useGeolocation/Geolocation";
 function App() {
   return (
     <>
       <div>
-        <CurrencyConverter />
+        <ReactQuiz />
       </div>
     </>
   );
---
+++ src/components/react-quiz/ReactQuiz.tsx
@@ -0,0 +1,11 @@
+import DateCounter from "./DateCounter";
+
+const ReactQuiz = () => {
+  return (
+    <div>
+      <DateCounter />
+    </div>
+  );
+};
+
+export default ReactQuiz;
